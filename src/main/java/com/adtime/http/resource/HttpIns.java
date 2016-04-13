417ac9e14a70fa3d38e8d1f822dbc4ee983fc060
lubin.xuan <lubin.xuan@gmail.com>
2016-04-13 11:43:29 +0800

    AsyncHttpClient

--- src/main/java/com/adtime/http/resource/HttpIns.java
+++ src/main/java/com/adtime/http/resource/HttpIns.java
@@ -29,49 +29,49 @@ public class HttpIns {
 
     private static final Map<Object, WebResource> WEB_RESOURCE_MAP = new ConcurrentHashMap<>();
 
-    public static WebResource httpClient() {
-        return WEB_RESOURCE_MAP.computeIfAbsent("httpClient", o -> httpClient(CRAWL_CONFIG));
+    public static HttpClientResource httpClient() {
+        return (HttpClientResource) WEB_RESOURCE_MAP.computeIfAbsent("httpClient", o -> httpClient(CRAWL_CONFIG));
     }
 
-    public static WebResource httpClient(CrawlConfig config) {
+    public static HttpClientResource httpClient(CrawlConfig config) {
         return newInstance(config, HttpClientResource.class, Clients435.class);
     }
 
 
-    public static WebResource asyncHttpClient() {
-        return WEB_RESOURCE_MAP.computeIfAbsent("asyncHttpClient", o -> httmluint(CRAWL_CONFIG));
+    public static AsyncHttpClient asyncHttpClient() {
+        return (AsyncHttpClient) WEB_RESOURCE_MAP.computeIfAbsent("asyncHttpClient", o -> httmluint(CRAWL_CONFIG));
     }
 
-    public static WebResource asyncHttpClient(CrawlConfig config) {
+    public static AsyncHttpClient asyncHttpClient(CrawlConfig config) {
         return newInstance(config, AsyncHttpClient.class);
     }
 
-    public static WebResource htmluint() {
-        return WEB_RESOURCE_MAP.computeIfAbsent("htmluint", o -> httmluint(CRAWL_CONFIG));
+    public static HttpUnitResource htmluint() {
+        return (HttpUnitResource) WEB_RESOURCE_MAP.computeIfAbsent("htmluint", o -> httmluint(CRAWL_CONFIG));
     }
 
-    public static WebResource httmluint(CrawlConfig config) {
+    public static HttpUnitResource httmluint(CrawlConfig config) {
         return newInstance(config, HttpUnitResource.class);
     }
 
-    public static WebResource httpUrlConnection() {
-        return WEB_RESOURCE_MAP.computeIfAbsent("httpUrlConnection", o -> httpUrlConnection(CRAWL_CONFIG));
+    public static HttpUrlConnectionResource httpUrlConnection() {
+        return (HttpUrlConnectionResource) WEB_RESOURCE_MAP.computeIfAbsent("httpUrlConnection", o -> httpUrlConnection(CRAWL_CONFIG));
     }
 
-    public static WebResource httpUrlConnection(CrawlConfig config) {
+    public static HttpUrlConnectionResource httpUrlConnection(CrawlConfig config) {
         return newInstance(config, HttpUrlConnectionResource.class);
     }
 
 
-    private static WebResource newInstance(CrawlConfig config, Class<? extends WebResource> clazz, Object... params) {
-        WebResource webResource;
+    private static <T extends WebResource> T newInstance(CrawlConfig config, Class<T> clazz, Object... params) {
+        T webResource;
         try {
             if (params.length > 0) {
                 Class[] classes = new Class[params.length];
                 for (int i = 0; i < params.length; i++) {
                     classes[i] = params[i].getClass();
                 }
-                Constructor<? extends WebResource> constructor = clazz.getConstructor(classes);
+                Constructor<T> constructor = clazz.getConstructor(classes);
                 webResource = constructor.newInstance(params);
             } else {
                 webResource = clazz.newInstance();
