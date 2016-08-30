d2fd42a48f720bf4f321b29a43ddae9f3c020a9b
xuanlubin <xlb900907>
2016-08-30 18:07:44 +0800

    HttpClient  Cookie 处理

--- src/main/java/com/adtime/http/resource/url/format/UrlFormatManager.java
+++ src/main/java/com/adtime/http/resource/url/format/UrlFormatManager.java
@@ -3,15 +3,21 @@ package com.adtime.http.resource.url.format;
 import com.adtime.http.resource.Request;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by xuanlubin on 2016/8/30.
  */
 public class UrlFormatManager<T> {
-    private List<UrlFormat<T>> urlFormatList;
+    private List<UrlFormat<T>> urlFormatList = new ArrayList<>();
 
     public void updateRequestParam(Request request, T t) {
+
+        if (urlFormatList.isEmpty()) {
+            return;
+        }
+
         for (UrlFormat<T> urlFormat : urlFormatList) {
             if (urlFormat.updateRequestParam(request, t)) {
                 return;
@@ -20,6 +26,11 @@ public class UrlFormatManager<T> {
     }
 
     public String format(String url, T t) {
+
+        if (urlFormatList.isEmpty()) {
+            return null;
+        }
+
         for (UrlFormat<T> urlFormat : urlFormatList) {
             String _url = urlFormat.format(url, t);
             if (StringUtils.isNotBlank(_url)) {
@@ -30,6 +41,16 @@ public class UrlFormatManager<T> {
     }
 
     public void setUrlFormatList(List<UrlFormat<T>> urlFormatList) {
-        this.urlFormatList = urlFormatList;
+        if (null == urlFormatList || urlFormatList.isEmpty()) {
+            return;
+        }
+        this.urlFormatList.addAll(urlFormatList);
+    }
+
+    public void addUrlFormat(UrlFormat<T> urlFormat) {
+        if (this.urlFormatList.contains(urlFormat)) {
+            return;
+        }
+        this.urlFormatList.add(urlFormat);
     }
 }
